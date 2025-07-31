11373bf57da4dc182d1242c2c2792a40c4186e19
shashankhr078 <shashankhr078@gmail.com>
2025-07-31 17:25:59 +0530

    5th commit:Implemented user authentication with JWT

--- server/server.js
+++ server/server.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
 
 mongoose.connect('mongodb://localhost:27017/logistics');
 
+app.use(cors());
 app.use(express.json());
+
+app.use('/api/auth', require('./routes/auth'));
+
 app.get('/', (req, res) => res.send('Server running'));
 app.listen(5000, () => console.log("Server started on port 5000"));
 
