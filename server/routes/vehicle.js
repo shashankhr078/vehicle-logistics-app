9214c6825e05b4803771a6914af436abb57a8f89
shashankhr078 <shashankhr078@gmail.com>
2025-08-01 15:13:05 +0530

    11th Commit: Added book.html with vehicle booking form

--- server/routes/vehicle.js
+++ server/routes/vehicle.js
@@ -16,4 +16,12 @@ router.put('/:id', updateVehicle);        // Update vehicle
 router.delete('/:id', deleteVehicle);     // Delete vehicle
 
 module.exports = router;
-
+router.get('/:id', async (req, res) => {
+  try {
+    const vehicle = await Vehicle.findById(req.params.id);
+    if (!vehicle) return res.status(404).send("Vehicle not found");
+    res.json(vehicle);
+  } catch (err) {
+    res.status(500).send("Server error");
+  }
+});
