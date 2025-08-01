a176ad05785ae746cd696192132f0b8f8bc528ad
shashankhr078 <shashankhr078@gmail.com>
2025-08-01 15:03:45 +0530

    10th Commit: Added vehicles.html to display vehicle list

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const cors = require('cors');
+const path = require('path');
 require('dotenv').config();
 
 mongoose.connect('mongodb://localhost:27017/logistics');
@@ -9,6 +10,7 @@ mongoose.connect('mongodb://localhost:27017/logistics');
 app.use(cors());
 app.use(express.json());
 
+app.use(express.static(path.join(__dirname, 'public')));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/vehicles', require('./routes/vehicle'));
 app.use('/api/bookings', require('./routes/booking'));
